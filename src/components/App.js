0d8f4ca290185bcc48c58e4f623e30e140c0aade
Shwe Yee Winn <yonngelay@gmail.com>
2019-06-20 13:19:00 +0800

    Displaying a video player

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,13 @@ import youtube from '../apis/youtube';
 
 import VideoList from './VideoList';
 
+import VideoDetail from './VideoDetail';
+
 class App extends Component {
   
   state = {
-    videos: []
+    videos: [],
+    selectedVideo: null
   };
 
   onTermSubmit = async term => {
@@ -25,11 +28,19 @@ class App extends Component {
 
   };
 
+  onVideoSelect = (video) => {
+    // console.log('From the app', video);
+    this.setState({
+      selectedVideo: video
+    })
+  }
+
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit}/>
-        <VideoList videos={this.state.videos}/>
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect}/>
       </div>
     );
   }
